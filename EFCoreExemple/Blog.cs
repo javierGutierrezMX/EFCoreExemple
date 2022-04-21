7e5b632c977c9d7ddf0838d832f8ad875c0b1de9
jgutierrez <jgutierrez@nexions.com>
2022-04-21 11:32:20 -0500

    Add project files.

---
+++ EFCoreExemple/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreExemple
+{
+    public class Blog
+    {
+        public int id { get; set; }
+        public string Url { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ EFCoreExemple/Data/BloggingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreExemple
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data source = (localdb)\MSSQLLocalDb;Initial Catalog = Blogging; Integrated Security = True");
+        }
+
+    }
+}
